ae18d6d91af3a6589d2bfda74052b833ca1f80ce
YamaGucY <yukita0612@icloud.com>
2023-11-27 16:54:50 +0900

    updata

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,9 @@
-import type { Config } from 'tailwindcss'
-
-const config: Config = {
+// tailwind.config.js
+module.exports = {
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/**/*.{js,ts,jsx,tsx}',
+    './public/index.html',
   ],
-  theme: {
-    extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
-    },
-  },
+  theme: {},
   plugins: [],
 }
-export default config
